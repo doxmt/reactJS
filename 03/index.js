5568f6fed3865ed8cf36ebf3005b571ca36b29a4
Songtaeha <songtaeha@naver.com>
2025-08-04 14:21:48 +0900

    라이브러리 사용방법

--- 03/index.js
+++ 03/index.js
@@ -1,4 +1,9 @@
 import mul, { add, sub } from "./math.js";
-console.log(add(8, 8));
-console.log(sub(8, 8));
-console.log(mul(5, 9));
+import randomColor from "randomcolor";
+
+const color = randomColor();
+console.log(color);
+
+// console.log(add(8, 8));
+// console.log(sub(8, 8));
+// console.log(mul(5, 9));
