694e22a7fa44465b8e9f2b8bdc61f3961bced1b3
Songtaeha <songtaeha@naver.com>
2025-08-06 19:30:38 +0900

    state,이벤트 핸들러 객체로 관리

--- 04/src/components/Register.jsx
+++ 04/src/components/Register.jsx
@@ -1,38 +1,43 @@
 import { useState } from "react";
 
 const Register = () => {
-  const [name, setName] = useState("");
-  const [birth, setBirth] = useState("");
-  const [country, setCountry] = useState("");
-  const [bio, setBio] = useState("");
+  const [input, setInput] = useState({
+    name: "",
+    birth: "",
+    country: "",
+    bio: "",
+  });
 
-  const onChangeName = (e) => {
-    setName(e.target.value);
+  const onChange = (e) => {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
   };
 
-  const onChangeBirth = (e) => {
-    setBirth(e.target.value);
-  };
-
-  const onChangeCountry = (e) => {
-    setCountry(e.target.value);
-  };
-
-  const onChangeBio = (e) => {
-    setBio(e.target.value);
-  };
+  console.log(input);
 
   return (
     <div>
       <div>
-        <input value={name} onChange={onChangeName} placeholder={"이름"} />
+        <input
+          name="name"
+          value={input.name}
+          onChange={onChange}
+          placeholder={"이름"}
+        />
       </div>
       <br />
       <div>
-        <input type="date" onChange={onChangeBirth} />
+        <input
+          name="birth"
+          value={input.birth}
+          type="date"
+          onChange={onChange}
+        />
       </div>
       <div>
-        <select value={country} onChange={onChangeCountry}>
+        <select name="country" value={input.country} onChange={onChange}>
           <option value=""></option>
           <option value="kr">한국</option>
           <option value="us">미국</option>
@@ -40,7 +45,12 @@ const Register = () => {
         </select>
       </div>
       <div>
-        <textarea placeholder="자기소개" onChange={onChangeBio}></textarea>
+        <textarea
+          name="bio"
+          value={input.bio}
+          placeholder="자기소개"
+          onChange={onChange}
+        ></textarea>
       </div>
     </div>
   );
