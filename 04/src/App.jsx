536929ac0f051f141c213005196a9c71326cb378
Songtaeha <songtaeha@naver.com>
2025-08-06 18:49:33 +0900

    state와 props

--- 04/src/App.jsx
+++ 04/src/App.jsx
@@ -1,28 +1,14 @@
 import "./App.css";
 import { useState } from 'react';
+import Bulb from './components/Bulb';
+import Counter from './components/Counter';
 
 function App() {
-  //리렌더링하기 위해 state사용
-  const [count, setCount] = useState(0);
-  const [light, setLight] = useState("off");
-
+  
   return (
     <>
-    <div>
-      <h1>{light}</h1>
-      <button onClick={()=>{
-        setLight(light === 'on'? "off": "on")
-      }}>
-        {light === 'on' ? "끄기" : "켜기"}
-        </button>
-    </div>
-
-
-    <div>
-    <h1>{count}</h1>
-    <button onClick={()=>{
-      setCount(count +1)}}>+</button>
-      </div>
+      <Bulb />
+      <Counter />
     </>
   );
 }
