80c4c53f4f16c36f99a55dd0f85796b1209eb743
Songtaeha <songtaeha@naver.com>
2025-08-11 15:35:57 +0900

    useEffect

---
+++ 07/src/App.jsx
@@ -0,0 +1,39 @@
+import "./App.css";
+import Viewer from "./Components/Viewer";
+import Controller from "./Components/Controller";
+import { useState, useEffect } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [input, setInput] = useState(0);
+
+  useEffect(() => {
+    console.log(`count: ${count} / input : ${input}`);
+  }, [count, input]);
+
+  const onClickButton = (value) => {
+    setCount(count + value);
+  };
+  return (
+    <div className="App">
+      <h1>Simple Counter</h1>
+      <section>
+        <input
+          value={input}
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
+        />
+      </section>
+      <section>
+        <Viewer count={count} />
+      </section>
+
+      <section>
+        <Controller onClickButton={onClickButton} />
+      </section>
+    </div>
+  );
+}
+
+export default App;
