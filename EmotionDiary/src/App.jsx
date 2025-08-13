f86940729ffe5961967d94398fc4919810998ebf
Songtaeha <songtaeha@naver.com>
2025-08-13 16:15:19 +0900

    공통 컴포넌트 구현 (버튼,헤더)

--- EmotionDiary/src/App.jsx
+++ EmotionDiary/src/App.jsx
@@ -5,6 +5,8 @@ import Diary from "./pages/Diary";
 import New from "./pages/New";
 import Notfound from "./pages/Notfound";
 import { getEmotionImage } from "./util/get-emotion-image";
+import Button from "./components/Button";
+import Header from "./components/Header";
 
 function App() {
   const nav = useNavigate();
@@ -13,19 +15,33 @@ function App() {
   };
   return (
     <>
-      <div>
-        <img src={getEmotionImage(1)} />
-        <img src={getEmotionImage(2)} />
-        <img src={getEmotionImage(3)} />
-        <img src={getEmotionImage(4)} />
-        <img src={getEmotionImage(5)} />
-      </div>
-      <div>
-        <Link to={"/"}>Home</Link>
-        <Link to={"/new"}>New</Link>
-        <Link to={"/diary"}>Diary</Link>
-      </div>
-      <button onClick={onClickButton}>New페이지로 이동</button>
+      <Header
+        title={"Header"}
+        leftChild={<Button text={"<"} />}
+        rightChild={<Button text={">"} />}
+      />
+
+      <Button
+        text={"기본버튼"}
+        type={""}
+        onClick={() => {
+          console.log("클릭됨");
+        }}
+      />
+      <Button
+        text={"긍정버튼"}
+        type={"POSITIVE"}
+        onClick={() => {
+          console.log("클릭됨");
+        }}
+      />
+      <Button
+        text={"부정버튼"}
+        type={"NEGATIVE"}
+        onClick={() => {
+          console.log("클릭됨");
+        }}
+      />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
