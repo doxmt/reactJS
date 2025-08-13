e07fa1afafc0b91b920603d0f3fbbb5577c1535c
Songtaeha <songtaeha@naver.com>
2025-08-13 15:23:03 +0900

    페이지 라우팅

--- EmotionDiary/src/App.jsx
+++ EmotionDiary/src/App.jsx
@@ -1,7 +1,31 @@
 import "./App.css";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import Home from "./pages/Home";
+import Diary from "./pages/Diary";
+import New from "./pages/New";
+import Notfound from "./pages/Notfound";
 
 function App() {
-  return <></>;
+  const nav = useNavigate();
+  const onClickButton = () => {
+    nav("/new");
+  };
+  return (
+    <>
+      <div>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/new"}>New</Link>
+        <Link to={"/diary"}>Diary</Link>
+      </div>
+      <button onClick={onClickButton}>New페이지로 이동</button>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/diary/:id" element={<Diary />} />
+        <Route path="*" element={<Notfound />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
