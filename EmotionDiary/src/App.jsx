d2f508eccd971e72d9e7e4daae4bfd57051ac4d4
Songtaeha <songtaeha@naver.com>
2025-08-13 16:34:52 +0900

    Edit페이지 추가

--- EmotionDiary/src/App.jsx
+++ EmotionDiary/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import New from "./pages/New";
+import Edit from "./pages/Edit";
 import Notfound from "./pages/Notfound";
 import { getEmotionImage } from "./util/get-emotion-image";
 import Button from "./components/Button";
@@ -15,37 +16,11 @@ function App() {
   };
   return (
     <>
-      <Header
-        title={"Header"}
-        leftChild={<Button text={"<"} />}
-        rightChild={<Button text={">"} />}
-      />
-
-      <Button
-        text={"기본버튼"}
-        type={""}
-        onClick={() => {
-          console.log("클릭됨");
-        }}
-      />
-      <Button
-        text={"긍정버튼"}
-        type={"POSITIVE"}
-        onClick={() => {
-          console.log("클릭됨");
-        }}
-      />
-      <Button
-        text={"부정버튼"}
-        type={"NEGATIVE"}
-        onClick={() => {
-          console.log("클릭됨");
-        }}
-      />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
         <Route path="/diary/:id" element={<Diary />} />
+        <Route path="/edit/:id" element={<Edit />} />
         <Route path="*" element={<Notfound />} />
       </Routes>
     </>
