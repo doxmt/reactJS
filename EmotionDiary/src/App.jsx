3a59140bfc52cf25e5bafa9c9a85968e11d07934
Songtaeha <songtaeha@naver.com>
2025-08-17 20:06:44 +0900

    웹 스토리지 적용

--- EmotionDiary/src/App.jsx
+++ EmotionDiary/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useReducer, useRef, createContext } from "react";
+import { useReducer, useRef, createContext, useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Diary from "./pages/Diary";
@@ -8,46 +8,66 @@ import Edit from "./pages/Edit";
 import Notfound from "./pages/Notfound";
 
 function reducer(state, action) {
+  let nextState;
   switch (action.type) {
-    case "CREATE":
-      return [action.data, ...state];
-    case "UPDATE":
-      return state.map((item) =>
+    case "INIT":
+      return action.data;
+    case "CREATE": {
+      nextState = [action.data, ...state];
+      break;
+    }
+    case "UPDATE": {
+      nextState = state.map((item) =>
         String(item.id) === String(action.data.id) ? action.data : item
       );
-    case "DELETE":
-      return state.filter((item) => String(item.id) !== String(action.id));
+      break;
+    }
+    case "DELETE": {
+      nextState = state.filter((item) => String(item.id) !== String(action.id));
+      break;
+    }
     default:
       return state;
   }
-}
 
-const mocData = [
-  {
-    id: 1,
-    createdDate: new Date("2025-08-15").getTime(),
-    emotionId: 1,
-    content: "1번일기입니다",
-  },
-  {
-    id: 2,
-    createdDate: new Date("2025-08-14").getTime(),
-    emotionId: 2,
-    content: "2번일기입니다",
-  },
-  {
-    id: 3,
-    createdDate: new Date("2025-07-15").getTime(),
-    emotionId: 3,
-    content: "4번일기입니다",
-  },
-];
+  localStorage.setItem("diary", JSON.stringify(nextState));
+  return nextState;
+}
 
 export const DiaryStateContext = createContext();
 export const DiaryDispatchContext = createContext();
 function App() {
-  const [data, dispatch] = useReducer(reducer, mocData);
-  const idRef = useRef(3);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const [data, dispatch] = useReducer(reducer, []);
+  const idRef = useRef();
+
+  useEffect(() => {
+    const storedData = localStorage.getItem("diary");
+    if (!storedData) {
+      setIsLoading(false);
+      return;
+    }
+    const parsedData = JSON.parse(storedData);
+    if (!Array.isArray(parsedData)) {
+      setIsLoading(false);
+      return;
+    }
+
+    let maxId = 0;
+    parsedData.forEach((item) => {
+      if (Number(item.id) > maxId) {
+        maxId = Number(item.id);
+      }
+    });
+
+    idRef.current = maxId + 1;
+    dispatch({
+      type: "INIT",
+      data: parsedData,
+    });
+    setIsLoading(false);
+  }, []);
 
   // 1. 일기 추가
   const onCreate = (createdDate, emotionId, content) => {
@@ -83,6 +103,9 @@ function App() {
     });
   };
 
+  if (isLoading) {
+    return <div>데이터 로딩 중 입니다...</div>;
+  }
   return (
     <>
       <DiaryStateContext.Provider value={data}>
